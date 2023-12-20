13b61113fb426f327b6f52389886b95b0e33a2f8
Ololade Sanya <ololadetimileyin3@gmail.com>
2023-12-20 08:32:06 +0100

    did all i had to to at this point made the design better but still working on it

--- src/component/ComingSoon.jsx
+++ src/component/ComingSoon.jsx
@@ -1,15 +1,33 @@
+import logo from '../images/CTC.png'
+import imgbg from '../images/computer-2583383_1280.jpg'
 import React from 'react'
+import iglogo from '../images/ig.svg'
+
+
+// /
 
 const ComingSoon = () => {
   return (
-    <div className=' dark:bg-black bg-white w-[100%] h-[100vh]  items-center justify-center mx-auto flex'>
-       <div className=' items-center justify-center w-full mx-auto flex flex-col leading-[40px] relative '>
-        <div className=' gradient '></div>
-           <h1 className=' text-6xl font-semibold dark:text-yellow-400 text-blue-900 space-y-8'>Coming soon</h1>
-            <p className=' dark:text-white to-blue-900 '>Crown Tech Circle</p>
-           <div className=' flex flex-row gap-4 items-center '>
+    <div className=' dark:bg-[#0e1024]  bg-white w-[100%] h-[100vh] gap-10  items-center justify-center mx-auto flex relative'>
+       <div className=' items-center justify-center w-full mx-auto flex flex-col leading-[40px]'>
+        <div className=' gradient absolute  '></div>
+        <div className=' gradient1 absolute w-10 h-10 right-0 top-0 '></div>
+        <div className=' gradient1 absolute w-10 h-10 left-0 bottom-0 '></div>
+        <img src={logo} alt="" className='absolute opacity-40 '/>
+        <h1 className='absolute opacity-20 text-8xl top-12 '>Crown Tech Citcle</h1>
+        <img src={imgbg} alt="" className='absolute opacity-10 z-10 w-full'/>
+        <img src={logo} alt="" className='absolute opacity-40 left-[-10px] top-[-15px] animate-spin-slow '/>
+        <img src={logo} alt="" className='absolute opacity-40  right-[-45px] bottom-[-35px]'/>
+        <div className=' gradient absolute right-[-45px] bottom-[-35px]  '></div>
+        <div className=' gradient absolute  left-[-45px] top-[-35px] animate-spin-slow hover:animate-none z-10'></div>
+           <h1 className=' text-6xl font-semibold dark:text-yellow-400 text-blue-900 space-y-8 z-50'>Coming soon</h1>
+            <p className=' dark:text-white to-blue-900 '>Empower, Trive, Innovate</p>
+           <form className=' z-50 flex flex-row gap-4 items-center '>
            <input className=' w-80 justify-center p-5 h-10 rounded-xl placeholder:flex placeholder:justify-center placeholder:text-slate-400 placeholder:text-md placeholder:pl-3 outline-none' type="email" placeholder='Join Waiting List' required />
-           <button type="submit" className=' text-yellow-200 bg-indigo-800 py-[1px] px-3  rounded-xl '></button>
+            <button type="submit" className=' text-yellow-200 bg-indigo-800 py-[1px] px-3  rounded-xl '>Join</button>
+           </form>
+           <div className=' flex flex-row gap-4 justify-center items-center mx-auto w-72 h-full'>
+              <img src={iglogo} className=' z-50' alt="" />
            </div>
        </div>
     </div>
